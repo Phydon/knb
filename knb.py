7d19e78c0b36d3b353e75b9a6aa46a3c55b313fe
Phydon <leann.phydon@gmail.com>
2024-09-28 15:25:03 +0200

    py init

--- knb.py
+++ knb.py
@@ -1,8 +1,63 @@
-import rich
+#!/usr/bin/env python
+
+from rich.console import Console  # V13.5.3
+from rich.markdown import Markdown
+from rich.table import Table
+from rich.tree import Tree
+
+# rich - colorful output
+console = Console(highlight=False)
+
+PATH_TO_KNOWLEDGE_BASE: str = "7z.md"
+# PATH_TO_KNOWLEDGE_BASE: str = "test.md"
 
 
 def main() -> None:
-    pass
+    content = read_file(PATH_TO_KNOWLEDGE_BASE)
+    console.print(Markdown(content))
+
+
+# def table() -> None:
+#     table = Table(
+#         title="Test",
+#         title_style="bold",
+#         show_lines=True,
+#     )
+
+#     table.add_column("Test1", justify="left", style="white")
+#     table.add_column("Test1", justify="right", style="dim italic")
+#     table.add_column("Test1", justify="right", style=" dim red")
+#     table.add_column("Test1", justify="right", style="dim blue")
+#     table.add_column("Test1", justify="right", style="blue")
+#     table.add_column("Test1", justify="right", style="blue")
+#     table.add_column("Test1", justify="right", style="red")
+#     table.add_column("Test1", justify="right", style="green")
+
+#     console.print(table)
+
+
+# def tree() -> None:
+#     tree = Tree("\n[b]Test[/b]")
+#     tree.add("[i]Test1[/i]: [blue]wasd[/blue]")
+#     tree.add("[i]Test2[/i]: [green]wasd[/green]")
+#     tree.add("[i]Test3[/i]: [red]wasd[/red]")
+#     console.print(tree)
+
+
+# def markdown() -> None:
+#     console.print(Markdown("# Test Heading"))
+#     console.print(Markdown("## Test Heading 2"))
+#     console.print(Markdown("### Test Heading 3"))
+#     console.print(Markdown("#### Test Heading 4"))
+#     console.print(Markdown("__Test__"))
+#     console.print(Markdown("_Test_"))
+
+
+def read_file(path: str) -> str:
+    with open(path, "r") as f:
+        content = f.read()
+
+    return content
 
 
 if __name__ == "__main__":
---
+++ test.md
@@ -0,0 +1,21 @@
+# Heading 1
+
+Lorem ipsum dolor sit amet
+
+## Heading 2
+
+_Lorem ipsum dolor sit amet_
+
+### Heading 3
+
+__Lorem ipsum dolor sit amet__
+
+#### Heading 4
+
+___Lorem ipsum dolor sit amet___
+
+##### Heading 5
+
+```shell
+Lorem ipsum dolor sit amet
+```
